214db51def82475296715ccd83a90c33ea6663e5
shuanghai.zhang <shuanghai.zhang@thoughtworks.com>
2020-02-15 22:07:36 +0800

    refactor: extract method to get final total amount

--- src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
+++ src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
@@ -16,8 +16,6 @@ public class OrderReceipt {
         output.append(order.getCustomerName());
         output.append(order.getCustomerAddress());
 
-        double totalSalesTax = 0d;
-        double finalTotalAmount = 0d;
         for (LineItem lineItem : order.getLineItems()) {
             output.append(lineItem.getDescription());
             output.append('\t');
@@ -27,16 +25,11 @@ public class OrderReceipt {
             output.append('\t');
             output.append(lineItem.totalAmount());
             output.append('\n');
-
-            double salesTax = lineItem.totalAmount() * .10;
-            totalSalesTax += salesTax;
-
-            finalTotalAmount += lineItem.totalAmount() + salesTax;
         }
 
         output.append("Sales Tax").append('\t').append(getTotalSalesTax(order));
 
-        output.append("Total Amount").append('\t').append(finalTotalAmount);
+        output.append("Total Amount").append('\t').append(getFinalTotalAmount(order));
         return output.toString();
     }
 
@@ -45,5 +38,12 @@ public class OrderReceipt {
                 .mapToDouble(LineItem::totalAmount)
                 .map(totalAmount -> totalAmount * 0.10)
                 .sum();
+    }
+
+    private double getFinalTotalAmount(Order order) {
+        double totalAmount = order.getLineItems().stream()
+                .mapToDouble(LineItem::totalAmount)
+                .sum();
+        return totalAmount + getTotalSalesTax(order);
     }
 }
